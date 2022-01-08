c58e3cd5dabf2610fffc65b2d0e3a5edd4f7c93a
ramusee <ram.u@live.com>
2022-01-09 01:56:09 +0400

    update ui

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ import { UI_ELEMENTS } from './view.js'
 UI_ELEMENTS.BTN.addEventListener('click', checkName)
 function checkName() {
   if (UI_ELEMENTS.NAME.value.trim() !== '') {
-    const firstName = UI_ELEMENTS.NAME.value.trim()
+    let firstName = UI_ELEMENTS.NAME.value.trim()
+    const currentName = firstName[0].toUpperCase() + firstName.slice(1)
     const serverNameUrl = 'https://api.genderize.io'
     const serverCountryUrl = 'https://api.nationalize.io'
     const urlName = `${serverNameUrl}?name=${firstName}`
@@ -13,20 +14,19 @@ function checkName() {
       .then((response) => response.json())
       .then((data) => {
         !data.gender
-          ? (UI_ELEMENTS.RESULT.textContent = `${firstName} is not found`)
-          : (UI_ELEMENTS.RESULT.textContent = `${firstName} is ${data.gender}`)
+          ? (UI_ELEMENTS.GENDER.textContent = `${currentName} not found`)
+          : (UI_ELEMENTS.GENDER.textContent = `${currentName} is ${data.gender}`)
       })
       .then(
         fetch(urlCountry)
           .then((response) => response.json())
           .then((data) => {
             const country = countries[data.country[0].country_id]
-            UI_ELEMENTS.RESULT.textContent = `${UI_ELEMENTS.RESULT.textContent} from ${country}`
+            UI_ELEMENTS.COUNTRY.textContent = `Country: ${country}`
+            console.log(country)
           })
-          .catch(
-            () =>
-              (UI_ELEMENTS.RESULT.textContent = `${UI_ELEMENTS.RESULT.textContent}. Сountry not found.`)
-          )
+
+          .catch(() => (UI_ELEMENTS.COUNTRY.textContent = 'Сountry not found'))
       )
   }
   UI_ELEMENTS.FORM.reset()
--- js/view.js
+++ js/view.js
@@ -1,6 +1,7 @@
 export const UI_ELEMENTS = {
+  FORM: document.querySelector('.form'),
   NAME: document.querySelector('.form__name'),
   BTN: document.querySelector('.form__btn'),
-  RESULT: document.querySelector('.form__result'),
-  FORM: document.querySelector('.form'),
+  GENDER: document.querySelector('.result__gender'),
+  COUNTRY: document.querySelector('.result__country'),
 }
